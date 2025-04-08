4abd2f1754778e0de4ccc431de8ace79504d9c34
Dani <thos.merida.dani@gmail.com>
2025-04-08 19:03:02 +0200

    Implementat el mapeig entre Categoria i CategoriaDTO amb MapStruct

--- botiga/src/main/java/com/accesadades/botiga/Mappers/CategoriaMapper.java
+++ botiga/src/main/java/com/accesadades/botiga/Mappers/CategoriaMapper.java
@@ -1,5 +1,27 @@
 package com.accesadades.botiga.Mappers;
 
-public class CategoriaMapper {
-    
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.ReportingPolicy;
+
+import java.util.Set;
+
+import com.accesadades.botiga.DTO.CategoriaDTO;
+import com.accesadades.botiga.Model.Categoria;
+
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface CategoriaMapper {
+
+    @Mapping(target = "desc_Categoria", source = "categoria.desc_Categoria")
+    @Mapping(target = "status_Categoria", source = "categoria.status_Categoria")
+    @Mapping(target = "subcategoria", source = "categoria.subcategoria")
+    CategoriaDTO CategoriaToCategoriaDTO(Categoria categoria);
+
+    @Mapping(target = "desc_Categoria", source = "categoriaDTO.desc_Categoria")
+    @Mapping(target = "status_Categoria", source = "categoriaDTO.status_Categoria")
+    @Mapping(target = "subcategoria", source = "categoriaDTO.subcategoria")
+    Categoria CategoriaDTOToCategoria(CategoriaDTO categoriaDTO);
+
+    Set<Categoria> CategoriesDTOToCategories(Set<CategoriaDTO> categoriesDTO);
+    Set<CategoriaDTO> CategoriesToCategoriesDTO(Set<Categoria> categories);
 }
