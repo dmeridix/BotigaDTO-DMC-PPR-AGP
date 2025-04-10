974adfb1a4054e4030de309e8bd56d9c2463df32
Pau Pedros <paupedros10@gmail.com>
2025-04-10 18:35:25 +0200

    Refactor ProductRepository to extend JpaRepository instead of CrudRepository

--- botiga/src/main/java/com/accesadades/botiga/Repository/ProductRepository.java
+++ botiga/src/main/java/com/accesadades/botiga/Repository/ProductRepository.java
@@ -1,18 +1,13 @@
 package com.accesadades.botiga.Repository;
 
 import org.springframework.stereotype.Repository;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.lang.NonNull;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Set;
 import com.accesadades.botiga.Model.Product;
 
 @Repository
-public interface ProductRepository extends CrudRepository<Product, Long> {
-
-    @Override
-    @NonNull
-    Set<Product> findAll();
+public interface ProductRepository extends JpaRepository<Product, Long> {
     Product findByName(String name);
     Set<Product> findByNameAndPrice(String name, float price);
 }
