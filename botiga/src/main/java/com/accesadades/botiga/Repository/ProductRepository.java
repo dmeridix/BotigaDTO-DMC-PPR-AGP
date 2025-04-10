ca53761c308b761db76703cd14bad28a6ee619df
Pau Pedros <paupedros10@gmail.com>
2025-04-10 18:36:09 +0200

    Change return type of findByNameAndPrice method from Set to List in ProductRepository

--- botiga/src/main/java/com/accesadades/botiga/Repository/ProductRepository.java
+++ botiga/src/main/java/com/accesadades/botiga/Repository/ProductRepository.java
@@ -3,11 +3,11 @@ package com.accesadades.botiga.Repository;
 import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Set;
+import java.util.List;
 import com.accesadades.botiga.Model.Product;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
     Product findByName(String name);
-    Set<Product> findByNameAndPrice(String name, float price);
+    List<Product> findByNameAndPrice(String name, float price);
 }
