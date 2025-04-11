b9a8c3d954705588cc9909d28185fec94753ef1d
Alex Gil <alexgpareja@icloud.com>
2025-04-11 18:23:56 +0200

    SubcategoriaRepo canviat el tipus a retornar

---
+++ botiga/src/main/java/com/accesadades/botiga/Service/SubcategoriaServiceImpl.java
@@ -0,0 +1,23 @@
+package com.accesadades.botiga.Service;
+
+import com.accesadades.botiga.DTO.SubcategoriaDTO;
+import com.accesadades.botiga.Mappers.SubcategoriaMapper;
+import com.accesadades.botiga.Model.Subcategoria;
+import com.accesadades.botiga.Repository.SubcategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+public class SubcategoriaServiceImpl implements GenericService<SubcategoriaDTO, Long> {
+
+    @Autowired
+    private SubcategoriaRepository subcategoriaRepository;
+
+    @Autowired
+    private SubcategoriaMapper subcategoriaMapper;
+
+}
