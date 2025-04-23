7b6a9a0ce6c834fdb50f55e5df7a9f333f176760
Alex Gil <alexgpareja@icloud.com>
2025-04-23 20:18:38 +0200

    Canvis en subcategoria per que funcioni el controlador (no funciona el insert)

--- botiga/src/main/java/com/accesadades/botiga/Mappers/SubcategoriaMapper.java
+++ botiga/src/main/java/com/accesadades/botiga/Mappers/SubcategoriaMapper.java
@@ -1,22 +1,21 @@
 package com.accesadades.botiga.Mappers;
 
 import java.util.List;
-
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
-
 import com.accesadades.botiga.DTO.SubcategoriaDTO;
 import com.accesadades.botiga.Model.Subcategoria;
 
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface SubcategoriaMapper {
 
+    @Mapping(target = "descCategoria", source = "subcategoria.categoria.descCategoria")
     SubcategoriaDTO subcategoriaToSubcategoriaDTO(Subcategoria subcategoria);
-    
+
     Subcategoria subcategoriaDTOToSubcategoria(SubcategoriaDTO subcategoriaDTO);
 
     List<SubcategoriaDTO> subcategoriesToSubcategoriesDTO(List<Subcategoria> subcategorias);
 
     List<Subcategoria> subcategoriesDTOToSubcategories(List<SubcategoriaDTO> subcategoriaDTOs);
-
 }
